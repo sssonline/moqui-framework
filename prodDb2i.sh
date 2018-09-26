655749ef8f4c07e3e33d372fb674e8707c369597
Mark Haney <mark@sssonline.com>
2018-09-26 10:57:05 -0700

    Add option to specify max memory as a parameter in integer form (i.e. 8, 16, etc). Defaults to 8 if not specified or invalid.

--- prodDb2i.sh
+++ prodDb2i.sh
@@ -1,3 +1,8 @@
 #! /usr/bin/env bash
-#java -server -XX:-OmitStackTraceInFastThrow -Xmx8192m -jar moqui.war conf=conf/MoquiProductionConfDb2i.xml
-java -XX:-OmitStackTraceInFastThrow -Xmx8192m -jar moqui.war conf=conf/MoquiProductionConfDb2i.xml
+MEM=8192
+RE='^[1-9][0-9]*$'
+if [[ $1 =~ $RE && $1 -le 128 ]]; then
+  let "MEM = $1 * 1024"
+fi
+#java -server -XX:-OmitStackTraceInFastThrow -Xmx${MEM}m -jar moqui.war conf=conf/MoquiProductionConfDb2i.xml
+java -XX:-OmitStackTraceInFastThrow -Xmx${MEM}m -jar moqui.war conf=conf/MoquiProductionConfDb2i.xml
--- prodH2.sh
+++ prodH2.sh
@@ -1,2 +1,7 @@
 #! /usr/bin/env bash
-java -server -XX:-OmitStackTraceInFastThrow -Xmx8192m -jar moqui.war conf=conf/MoquiProductionConf.xml
+MEM=8192
+RE='^[1-9][0-9]*$'
+if [[ $1 =~ $RE && $1 -le 128 ]]; then
+  let "MEM = $1 * 1024"
+fi
+java -server -XX:-OmitStackTraceInFastThrow -Xmx${MEM}m -jar moqui.war conf=conf/MoquiProductionConf.xml
