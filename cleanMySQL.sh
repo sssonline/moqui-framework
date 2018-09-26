49467dbe4bb5e6fe62ae4bcb5783badff8db4578
Mark Haney <mark@sssonline.com>
2018-09-26 10:16:42 -0700

    Remove inline password in favor of ~/.my.cnf file

--- cleanMySQL.sh
+++ cleanMySQL.sh
@@ -1,1 +1,1 @@
-mysql -u root -pmoqui1! -e "DROP DATABASE IF EXISTS moqui; CREATE DATABASE moqui; GRANT ALL PRIVILEGES ON *.* TO 'moquiJoeUser'@'%';"
+mysql -e "DROP DATABASE IF EXISTS moqui; CREATE DATABASE moqui; GRANT ALL PRIVILEGES ON *.* TO 'moquiJoeUser'@'%';"
--- debugMySQL.sh
+++ debugMySQL.sh
@@ -1,1 +1,1 @@
-mysql -u root -pmoqui1! -e "UPDATE performance_schema.setup_instruments SET enabled = 'YES' WHERE name = 'wait/lock/metadata/sql/mdl'; SELECT * FROM performance_schema.metadata_locks where LOCK_TYPE <> 'SHARED_READ';"
+mysql -e "UPDATE performance_schema.setup_instruments SET enabled = 'YES' WHERE name = 'wait/lock/metadata/sql/mdl'; SELECT * FROM performance_schema.metadata_locks where LOCK_TYPE <> 'SHARED_READ';"
