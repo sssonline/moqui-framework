95c8f2ff85389fd354db0a1f8ea0f8fa89130012
Solomon Bessire <sib@sssonline.com>
2018-11-18 12:44:48 -0800

    Don't load webroot for gebbers demo data.

--- gebbersH2.sh
+++ gebbersH2.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 gradle cleanAll
 gradle load -Ptypes=seed,seed-initial,install
-java -XX:+UseG1GC -jar moqui.war load types=demo components=webroot,gebbers
+java -XX:+UseG1GC -jar moqui.war load types=demo components=gebbers
 gradle runtime:component:gebbers:test
