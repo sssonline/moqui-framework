2affae580e5f4a2a17b1a63fbc8c9d119ff0a763
Solomon Bessire <sib@sssonline.com>
2019-04-30 14:38:52 -0700

    For now, don't log invalid ETL entries.  If the desire is to just use the E and T part of it, without the load, don't issue all the log messages on the failure to load the entity.

--- framework/src/main/groovy/org/moqui/impl/entity/EntityFacadeImpl.groovy
+++ framework/src/main/groovy/org/moqui/impl/entity/EntityFacadeImpl.groovy
@@ -1,12 +1,12 @@
 /*
  * This software is in the public domain under CC0 1.0 Universal plus a
  * Grant of Patent License.
- * 
+ *
  * To the extent possible under law, the author(s) have dedicated all
  * copyright and related and neighboring rights to this software to the
  * public domain worldwide. This software is distributed without any
  * warranty.
- * 
+ *
  * You should have received a copy of the CC0 Public Domain Dedication
  * along with this software (see the LICENSE.md file). If not, see
  * <http://creativecommons.org/publicdomain/zero/1.0/>.
@@ -1976,7 +1976,7 @@ class EntityFacadeImpl implements EntityFacade {
         @Override void load(SimpleEtl.Entry entry) throws Exception {
             String entityName = entry.getEtlType()
             if (!efi.isEntityDefined(entityName)) {
-                logger.info("Tried to load ETL entry with invalid entity name " + entityName)
+//                logger.info("Tried to load ETL entry with invalid entity name " + entityName)
                 return
             }
             EntityDefinition ed = efi.getEntityDefinition(entityName)
