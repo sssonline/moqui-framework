9692bc21b2372241a1fd4ecdeb78013a0b686e0e
Solomon Bessire <sib@sssonline.com>
2019-04-30 09:27:49 -0700

    Also comment out the demo data load.

--- gebbersNoTestsH2.sh
+++ gebbersNoTestsH2.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/env bash
 gradle cleanAll
 gradle load -Ptypes=seed,seed-initial,install
-java -XX:+UseG1GC -jar moqui.war load types=demo components=gebbers
+#java -XX:+UseG1GC -jar moqui.war load types=demo components=gebbers
+#gradle runtime:component:gebbers:test
