efc97cdb44a5da33ffb61c253cff4f02acf87c8a
Solomon Bessire <sib@sssonline.com>
2019-05-28 10:11:03 -0700

    Remove old one.

--- gebbersNoTestsH2.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env bash
-gradle cleanAll
-gradle load -Ptypes=seed,seed-initial,install
-#java -XX:+UseG1GC -jar moqui.war load types=demo components=gebbers
-#gradle runtime:component:gebbers:test
