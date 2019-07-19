94d6f2ad31386b31c6bf978c0348f894aff9b4fc
Solomon Bessire <sib@sssonline.com>
2019-07-19 11:38:53 -0700

    Move the John doe data to basalt for selective load.

--- gebbersH2.sh
+++ gebbersH2.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/env bash
 gradle cleanAll
 gradle load -Ptypes=seed,seed-initial,install
-java -XX:+UseG1GC -jar moqui.war load types=johndoe,demo components=gebbers
+java -XX:+UseG1GC -jar moqui.war load types=johndoe components=basalt
+java -XX:+UseG1GC -jar moqui.war load types=demo components=gebbers
 gradle runtime:component:gebbers:test
