fb45b3bc9c1a861c345716454c1ad53ca9722706
Solomon Bessire <sib@sssonline.com>
2019-07-29 11:30:06 -0700

    Rename to gebbers farms.

---
+++ gebbersFarmsNoDemoH2.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env bash
+gradle cleanAll
+gradle load -Ptypes=seed,seed-initial,install
+java -XX:+UseG1GC -jar moqui.war load types=seed-gebbersCommon,seed-initial-gebbersCommon,install-gebbersCommon,seed-gebbersFarms,seed-initial-gebbersFarms,install-gebbersFarms components=gebbers
+java -XX:+UseG1GC -jar moqui.war load types=johndoe-gebbersFarms components=gebbers
+#java -XX:+UseG1GC -jar moqui.war load types=demo-gebbersFarms components=gebbers
+#gradle runtime:component:gebbers:test
