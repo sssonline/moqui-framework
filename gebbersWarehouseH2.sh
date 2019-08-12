4887b426898f5bc34fdd2639d2bb3a980d6b2caa
Solomon Bessire <sib@sssonline.com>
2019-08-12 11:39:13 -0700

    Initial warehouse scripts.

---
+++ gebbersWarehouseH2.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env bash
+gradle cleanAll
+gradle load -Ptypes=seed,seed-initial,install
+java -XX:+UseG1GC -jar moqui.war load types=seed-gebbersCommon,seed-initial-gebbersCommon,install-gebbersCommon,seed-gebbersWarehouse,seed-initial-gebbersWarehouse,install-gebbersWarehouse components=gebbers
+java -XX:+UseG1GC -jar moqui.war load types=johndoe-gebbersWarehouse components=gebbers
+java -XX:+UseG1GC -jar moqui.war load types=demo-gebbersWarehouse components=gebbers
+gradle runtime:component:gebbers:test
