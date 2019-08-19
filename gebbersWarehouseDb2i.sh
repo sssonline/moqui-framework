1d87036bb0b40d21f86329a49bd40514f3e67761
Solomon Bessire <sib@sssonline.com>
2019-08-19 08:18:16 -0700

    Don't run the gebbers tests for now.

--- gebbersWarehouseDb2i.sh
+++ gebbersWarehouseDb2i.sh
@@ -5,4 +5,4 @@ gradle load -Ptypes=seed,seed-initial,install -Pconf=conf/MoquiDevConfDb2i.xml -
 java -XX:+UseG1GC -jar moqui.war load types=seed-gebbersCommon,seed-initial-gebbersCommon,install-gebbersCommon,seed-gebbersWarehouse,seed-initial-gebbersWarehouse,install-gebbersWarehouse components=gebbers conf=conf/MoquiDevConfDb2i.xml -Duser.dir=`pwd`
 java -XX:+UseG1GC -jar moqui.war load types=johndoe-gebbersWarehouse components=gebbers conf=conf/MoquiDevConfDb2i.xml -Duser.dir=`pwd`
 java -XX:+UseG1GC -jar moqui.war load types=demo-gebbersWarehouse components=gebbers conf=conf/MoquiDevConfDb2i.xml -Duser.dir=`pwd`
-gradle runtime:component:gebbers:test -Pconf=conf/MoquiDevConfDb2i.xml -Duser.dir=`pwd`
+#gradle runtime:component:gebbers:test -Pconf=conf/MoquiDevConfDb2i.xml -Duser.dir=`pwd`
