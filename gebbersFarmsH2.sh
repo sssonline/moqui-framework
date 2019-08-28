5fc915d8e30c6ee54fb64e9075424e1129fd5fa3
Kira Miller <kiramiller425@gmail.com>
2019-08-28 16:52:11 -0700

    added helpful comment

--- gebbersFarmsH2.sh
+++ gebbersFarmsH2.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/env bash
 gradle cleanAll
+# See build.gradle to see how it loads this seed data:
 gradle load -Ptypes=seed,seed-initial,install
 java -XX:+UseG1GC -jar moqui.war load types=seed-gebbersCommon,seed-initial-gebbersCommon,install-gebbersCommon,seed-gebbersFarms,seed-initial-gebbersFarms,install-gebbersFarms components=gebbers
 java -XX:+UseG1GC -jar moqui.war load types=johndoe-gebbersFarms components=gebbers
