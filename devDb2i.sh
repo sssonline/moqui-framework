5befc5508f2746ddc4c15f88515c3049727f9490
Mark Haney <mark@sssonline.com>
2019-09-17 11:35:43 -0700

    Add comment example for starting server on different ports

--- devDb2i.sh
+++ devDb2i.sh
@@ -5,4 +5,5 @@ if [[ $1 =~ $RE && $1 -le 128 ]]; then
   let "MEM = $1 * 1024"
 fi
 #java -server -Djava.awt.headless=true -XX:-OmitStackTraceInFastThrow -XX:+UseG1GC -Xmx${MEM}m -jar moqui.war conf=conf/MoquiDevConfDb2i.xml
+#java -Djava.awt.headless=true -XX:-OmitStackTraceInFastThrow -XX:+UseG1GC -Xmx${MEM}m -jar moqui.war conf=conf/MoquiDevConfDb2i.xml port=8081
 java -Djava.awt.headless=true -XX:-OmitStackTraceInFastThrow -XX:+UseG1GC -Xmx${MEM}m -jar moqui.war conf=conf/MoquiDevConfDb2i.xml
