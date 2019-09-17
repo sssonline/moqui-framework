4b5c860ce30a34825f208920e20f47ce160b6fe1
Mark Haney <mark@sssonline.com>
2019-09-17 14:19:00 -0700

    Update load and startup scripts to use custom configuration files for Gebbers Warehouse.

--- gebbersWarehouseNoDemoDb2i.sh
+++ gebbersWarehouseNoDemoDb2i.sh
@@ -1,9 +1,9 @@
 #! /usr/bin/env bash
 gradle cleanAllButDb -Duser.dir=`pwd`
 # CLRLIB MOQUI
-gradle load -Ptypes=seed,seed-initial,install -Pconf=conf/MoquiDevConfDb2i.xml -Duser.dir=`pwd`
-java -XX:+UseG1GC -jar moqui.war load types=seed-gebbersCommon,seed-initial-gebbersCommon,install-gebbersCommon components=gebbers conf=conf/MoquiDevConfDb2i.xml -Duser.dir=`pwd`
-java -XX:+UseG1GC -jar moqui.war load types=seed-gebbersWarehouse,seed-initial-gebbersWarehouse,install-gebbersWarehouse components=gebbersw conf=conf/MoquiDevConfDb2i.xml -Duser.dir=`pwd`
-java -XX:+UseG1GC -jar moqui.war load types=johndoe-gebbersWarehouse components=gebbersw conf=conf/MoquiDevConfDb2i.xml -Duser.dir=`pwd`
-#java -XX:+UseG1GC -jar moqui.war load types=demo-gebbersWarehouse components=gebbersw conf=conf/MoquiDevConfDb2i.xml -Duser.dir=`pwd`
-#gradle runtime:component:gebbersw:test -Pconf=conf/MoquiDevConfDb2i.xml -Duser.dir=`pwd`
+gradle load -Ptypes=seed,seed-initial,install -Pconf=conf/MoquiDevConfDb2iPayrollGw.xml -Duser.dir=`pwd`
+java -XX:+UseG1GC -jar moqui.war load types=seed-gebbersCommon,seed-initial-gebbersCommon,install-gebbersCommon components=gebbers conf=conf/MoquiDevConfDb2iPayrollGw.xml -Duser.dir=`pwd`
+java -XX:+UseG1GC -jar moqui.war load types=seed-gebbersWarehouse,seed-initial-gebbersWarehouse,install-gebbersWarehouse components=gebbersw conf=conf/MoquiDevConfDb2iPayrollGw.xml -Duser.dir=`pwd`
+java -XX:+UseG1GC -jar moqui.war load types=johndoe-gebbersWarehouse components=gebbersw conf=conf/MoquiDevConfDb2iPayrollGw.xml -Duser.dir=`pwd`
+#java -XX:+UseG1GC -jar moqui.war load types=demo-gebbersWarehouse components=gebbersw conf=conf/MoquiDevConfDb2iPayrollGw.xml -Duser.dir=`pwd`
+#gradle runtime:component:gebbersw:test -Pconf=conf/MoquiDevConfDb2iPayrollGw.xml -Duser.dir=`pwd`
