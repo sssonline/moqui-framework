706f2dbc1c876f784bd1d4e62fbe63cf8dfd2bf3
Solomon Bessire <sib@sssonline.com>
2019-09-17 09:11:57 -0700

    Split loading a little further to see if it helps run against iSeries.

--- gebbersWarehouseNoDemoDb2i.sh
+++ gebbersWarehouseNoDemoDb2i.sh
@@ -2,7 +2,8 @@
 gradle cleanAllButDb -Duser.dir=`pwd`
 # CLRLIB MOQUI
 gradle load -Ptypes=seed,seed-initial,install -Pconf=conf/MoquiDevConfDb2i.xml -Duser.dir=`pwd`
-java -XX:+UseG1GC -jar moqui.war load types=seed-gebbersCommon,seed-initial-gebbersCommon,install-gebbersCommon,seed-gebbersWarehouse,seed-initial-gebbersWarehouse,install-gebbersWarehouse components=gebbers,gebbersw conf=conf/MoquiDevConfDb2i.xml -Duser.dir=`pwd`
+java -XX:+UseG1GC -jar moqui.war load types=seed-gebbersCommon,seed-initial-gebbersCommon,install-gebbersCommon components=gebbers conf=conf/MoquiDevConfDb2i.xml -Duser.dir=`pwd`
+java -XX:+UseG1GC -jar moqui.war load types=seed-gebbersWarehouse,seed-initial-gebbersWarehouse,install-gebbersWarehouse components=gebbersw conf=conf/MoquiDevConfDb2i.xml -Duser.dir=`pwd`
 java -XX:+UseG1GC -jar moqui.war load types=johndoe-gebbersWarehouse components=gebbersw conf=conf/MoquiDevConfDb2i.xml -Duser.dir=`pwd`
 #java -XX:+UseG1GC -jar moqui.war load types=demo-gebbersWarehouse components=gebbersw conf=conf/MoquiDevConfDb2i.xml -Duser.dir=`pwd`
 #gradle runtime:component:gebbersw:test -Pconf=conf/MoquiDevConfDb2i.xml -Duser.dir=`pwd`
