6e4a0a049b21d99958aa73c347747ff317822998
Mark Haney <mark@sssonline.com>
2019-09-18 08:41:45 -0700

    Custom configurations for Gebbers Farms, similar to Gebbers Warehouse.

--- gebbersFarmsDb2i.sh
+++ gebbersFarmsDb2i.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env bash
 gradle cleanAllButDb -Duser.dir=`pwd`
 # CLRLIB MOQUI
-gradle load -Ptypes=seed,seed-initial,install -Pconf=conf/MoquiDevConfDb2i.xml -Duser.dir=`pwd`
-java -XX:+UseG1GC -jar moqui.war load types=seed-gebbersCommon,seed-initial-gebbersCommon,install-gebbersCommon,seed-gebbersFarms,seed-initial-gebbersFarms,install-gebbersFarms components=gebbers conf=conf/MoquiDevConfDb2i.xml -Duser.dir=`pwd`
-java -XX:+UseG1GC -jar moqui.war load types=johndoe-gebbersFarms components=gebbers conf=conf/MoquiDevConfDb2i.xml -Duser.dir=`pwd`
-java -XX:+UseG1GC -jar moqui.war load types=demo-gebbersFarms components=gebbers conf=conf/MoquiDevConfDb2i.xml -Duser.dir=`pwd`
-gradle runtime:component:gebbers:test -Pconf=conf/MoquiDevConfDb2i.xml -Duser.dir=`pwd` -Darg1=GebbersFarms
+gradle load -Ptypes=seed,seed-initial,install -Pconf=conf/MoquiDevConfDb2iPayrollGf.xml -Duser.dir=`pwd`
+java -XX:+UseG1GC -jar moqui.war load types=seed-gebbersCommon,seed-initial-gebbersCommon,install-gebbersCommon,seed-gebbersFarms,seed-initial-gebbersFarms,install-gebbersFarms components=gebbers conf=conf/MoquiDevConfDb2iPayrollGf.xml -Duser.dir=`pwd`
+java -XX:+UseG1GC -jar moqui.war load types=johndoe-gebbersFarms components=gebbers conf=conf/MoquiDevConfDb2iPayrollGf.xml -Duser.dir=`pwd`
+java -XX:+UseG1GC -jar moqui.war load types=demo-gebbersFarms components=gebbers conf=conf/MoquiDevConfDb2iPayrollGf.xml -Duser.dir=`pwd`
+gradle runtime:component:gebbers:test -Pconf=conf/MoquiDevConfDb2iPayrollGf.xml -Duser.dir=`pwd` -Darg1=GebbersFarms
