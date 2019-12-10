d4670e8de0c1d7ebaa624b61cb4ce5bfe676092e
Kira Miller <kiramiller425@gmail.com>
2019-12-09 16:55:53 -0800

    created new user jane doe and facility user role

--- gebbersWarehouseH2.sh
+++ gebbersWarehouseH2.sh
@@ -3,5 +3,6 @@ gradle cleanAll
 gradle load -Ptypes=seed,seed-initial,install
 java -XX:+UseG1GC -jar moqui.war load types=seed-gebbersCommon,seed-initial-gebbersCommon,install-gebbersCommon,seed-gebbersWarehouse,seed-initial-gebbersWarehouse,install-gebbersWarehouse components=gebbers,gebbersw
 java -XX:+UseG1GC -jar moqui.war load types=johndoe-gebbersWarehouse components=gebbersw
+java -XX:+UseG1GC -jar moqui.war load types=janedoe-gebbersWarehouse components=gebbersw
 java -XX:+UseG1GC -jar moqui.war load types=demo-gebbersWarehouse components=gebbersw
 gradle runtime:component:gebbersw:test # -Darg1=GebbersWarehouse
